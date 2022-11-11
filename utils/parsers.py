ab1e872964548052b51951fb656a2db89589dae1
1nfernoS <gaydenko01@yandex.ru>
2022-11-12 02:50:17 +0300

    Add storage action parse

--- utils/parsers.py
+++ utils/parsers.py
@@ -1,11 +1,13 @@
 from datetime import datetime
+import re
 
-from DB.items import get_item_by_name
+from config import DISCOUNT_PERCENT
+
+from DB.items import get_item_by_name, search_item
 
 
 def parse_profile(text: str) -> dict:
     text = text.encode('cp1251', 'xmlcharrefreplace').decode('cp1251')
-    import re
     officer_emoji = '&#11088;'
     t = text.split('\n')
 
@@ -34,4 +36,43 @@ def parse_profile(text: str) -> dict:
            'level': level, 'strength': strength, 'agility': agility, 'endurance': endurance, 'luck': luck,
            'attack': attack, 'defence': defence, 'last_update': datetime.now(), 'class_name': class_name,
            'race': race, 'name': name}
+    return res
+
+
+def parse_storage_action(text: str):
+    import profile_api
+    text = text.encode('cp1251', 'xmlcharrefreplace').decode('cp1251')
+    res = {}
+
+    id_vk = int(re.findall(r'(?<=id)\d+', text)[0])
+
+    if '&#128213;' in text or '&#128216;' in text:
+        res = {'item_type': 'book'}
+
+        count = int(re.findall(r'(?<=&#128216;|&#128213;)\d+(?=\*)', text)[0])
+        item_name = re.findall(r'(?<=\*)\D+(?=!)', text)[0]
+
+        item_id = search_item(item_name)['result'][0]['item_id']
+
+        if not item_id:
+            return
+
+        price = profile_api.price(item_id)
+        result_price = round(price * (100 - DISCOUNT_PERCENT) / 100)
+        res.update({'id_vk': id_vk, 'count': count, 'item_name': item_name, 'price': price})
+
+        if 'взяли' in text:
+            res['result_price'] = -result_price
+        if 'положили' in text:
+            res['result_price'] = result_price
+
+    if 'золота' in text:
+        res = {'item_type': 'gold'}
+
+        count = int(re.findall(r'\d+(?= золота)', text)[0])
+
+        if 'взяли' in text:
+            res.update({'id_vk': id_vk, 'count': -count})
+        if 'положили' in text:
+            res.update({'id_vk': id_vk, 'count': count})
     return res
