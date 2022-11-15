04aef60531ed676cab6dd9b3e86525b66ba39958
1nfernoS <gaydenko01@yandex.ru>
2022-11-15 22:00:30 +0300

    Moved link in if clause

--- handlers/new_message/direct_messages.py
+++ handlers/new_message/direct_messages.py
@@ -35,32 +35,32 @@ def user_message(self: VkBot, event: VkBotEvent):
         ans = 'Пару секунд, мне нужно изучить твои статы и экипировку... Один раз'
         self.api.send_user_msg(event.message.from_id, ans)
 
-    s = event.message.text[event.message.text.find('act='):event.message.text.find('&group_id')]
-    auth = s[s.find('auth_key') + 9:s.find('auth_key') + 41]
-    profile = get_profile(auth, event.message.from_id)
+        s = event.message.text[event.message.text.find('act='):event.message.text.find('&group_id')]
+        auth = s[s.find('auth_key') + 9:s.find('auth_key') + 41]
+        profile = get_profile(auth, event.message.from_id)
 
-    stats = profile['stats']
-    new_data = (event.message.from_id,
-                stats['level'], stats['attack'], stats['defence'],
-                stats['strength'], stats['agility'], stats['endurance'],
-                stats['luck'], stats['accuracy'], stats['concentration'])
+        stats = profile['stats']
+        new_data = (event.message.from_id,
+                    stats['level'], stats['attack'], stats['defence'],
+                    stats['strength'], stats['agility'], stats['endurance'],
+                    stats['luck'], stats['accuracy'], stats['concentration'])
 
-    user_data.update_user_data(*new_data)
+        user_data.update_user_data(*new_data)
 
-    inv = [int(i) for i in profile['items']]
-    class_id = inv[0] if inv[0] != 14108 else inv[1]
-    build = get_books(inv)
-    data_old = users.get_user(event.message.from_id)
-    if data_old:
-        users.update_user(event.message.from_id, profile_key=auth, is_active=1, equipment=json.dumps(build),
-                          class_id=class_id)
-        ans = f"Обновил твой профиль! Значит, твой высший класс {get_item_by_id(class_id)}?\n" \
-              f"Похвально, я буду сообщать о тебе, когда твои книги будут на продаже"
-    else:
-        users.add_user(event.message.from_id, auth, True, False, False, json.dumps(build), class_id)
-        ans = f"В первый раз, значит? Что ж, проходи, сейчас запишу... \n" \
-              f"Так, высший класс {get_item_by_id(class_id)}, хорошо...\n" \
-              f"Готово, теперь я буду сообщать о тебе, когда твои книги будут на продаже"
+        inv = [int(i) for i in profile['items']]
+        class_id = inv[0] if inv[0] != 14108 else inv[1]
+        build = get_books(inv)
+        data_old = users.get_user(event.message.from_id)
+        if data_old:
+            users.update_user(event.message.from_id, profile_key=auth, is_active=1, equipment=json.dumps(build),
+                              class_id=class_id)
+            ans = f"Обновил твой профиль! Значит, твой высший класс {get_item_by_id(class_id)}?\n" \
+                  f"Похвально, я буду сообщать о тебе, когда твои книги будут на продаже"
+        else:
+            users.add_user(event.message.from_id, auth, True, False, False, json.dumps(build), class_id)
+            ans = f"В первый раз, значит? Что ж, проходи, сейчас запишу... \n" \
+                  f"Так, высший класс {get_item_by_id(class_id)}, хорошо...\n" \
+                  f"Готово, теперь я буду сообщать о тебе, когда твои книги будут на продаже"
 
-    self.api.send_user_msg(event.message.from_id, ans)
+        self.api.send_user_msg(event.message.from_id, ans)
     return
