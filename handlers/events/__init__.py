3bca8cc9f1833ee5d519407f0e6638d0dbda449a
1nfernoS <gaydenko01@yandex.ru>
2023-01-06 03:13:32 +0300

    Moved events into a package

---
+++ handlers/events/__init__.py
@@ -0,0 +1,55 @@
+import json
+
+from vk_api.bot_longpoll import VkBotEvent
+
+from vk_bot.vk_bot import VkBot
+
+from .buffs import buff
+
+
+def event_message(self: VkBot, event: VkBotEvent):
+    pl = event.object.payload
+    action = pl.get('action')
+    if action:
+        if action == 'buff':
+
+            self.api.send_event(
+                event.object.peer_id,
+                event.object.event_id,
+                event.object.user_id,
+                json.dumps({'type': 'show_snackbar', 'text': 'Накладываю бафф...'})
+            )
+
+            res = buff(pl['from'], pl['chat_id'], pl['msg_id'], pl['buff'])
+
+            self.api.edit_msg(
+                event.object.peer_id,
+                event.object.conversation_message_id,
+                res
+            )
+            return
+        else:
+            self.api.send_event(
+                event.object.peer_id,
+                event.object.event_id,
+                event.object.user_id,
+                json.dumps({'type': 'show_snackbar', 'text': 'Что-то пошло не так... Упс?'})
+            )
+            self.api.del_msg(
+                event.object.peer_id,
+                self.api.get_conversation_msg(event.object.peer_id, event.object.conversation_message_id)['id']
+            )
+            return
+    else:
+        self.api.send_event(
+            event.object.peer_id,
+            event.object.event_id,
+            event.object.user_id,
+            json.dumps({'type': 'show_snackbar', 'text': 'Что-то пошло не так... Упс?'})
+        )
+        self.api.del_msg(
+            event.object.peer_id,
+            self.api.get_conversation_msg(event.object.peer_id, event.object.conversation_message_id)['id']
+        )
+        return
+    return
