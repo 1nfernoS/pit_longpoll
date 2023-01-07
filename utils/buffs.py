5c0a5da36d3523fbe6ef4db92591ad97b597e3c5
1nfernoS <gaydenko01@yandex.ru>
2023-01-07 23:16:35 +0300

    Added all buff classes

--- utils/buffs.py
+++ utils/buffs.py
@@ -10,3 +10,7 @@ POSSIBLE_ANSWERS = ('через определенное время', 'уже д
                     'требуется Голос древних')
 
 APOSTOL_ITEM_ID = 14264
+WARLOCK_ITEM_ID = 14093
+PALADIN_ITEM_ID = 14088
+CRUSADER_ITEM_ID = 14256
+LIGHT_INC_ITEM_ID = 14257
