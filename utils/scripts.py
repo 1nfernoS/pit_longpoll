deffc7f3421ac4fba70f89b9f08eddf33cf23dbf
1nfernoS <gaydenko1801@gmail.ru>
2023-02-02 09:12:34 +0300

    Refactor code^ simplify

--- utils/scripts.py
+++ utils/scripts.py
@@ -8,20 +8,16 @@ import DB.user_data as user_data
 
 def withdraw_bill(bot: VkBot) -> None:
     members = bot.api.get_members(GUILD_CHAT_ID)
-    # print(members, end=', ')
     for user_id in members:
         print(user_id)
         if user_id < 0:
             continue
         data_user = user_data.get_user_data(user_id)
-        if data_user:
-            # print(data_user['level'])
-            if data_user['level'] < 100:
-                users.change_balance(user_id, -18000)
-            else:
-                users.change_balance(user_id, -36000)
-    # bot.api.send_chat_msg(GUILD_CHAT_ID, 'С баланса списан месячный налог, проверить свой баланс можно командой баланс')
-    print('Done')
+        if not data_user:
+            continue
+        users.change_balance(user_id,
+                             -18000 if data_user['level'] < 100
+                             else -36000)
     return
 
 
