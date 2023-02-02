fb55cc4f75de7886bf0beb1dba3b64c839ac1a4a
1nfernoS <gaydenko1801@gmail.ru>
2023-02-02 09:02:34 +0300

    Refactored code

--- commands/user_commands.py
+++ commands/user_commands.py
@@ -67,7 +67,7 @@ class Help(Command):
                 if not command_list[cmd].require_creator and not command_list[cmd].require_leader and not command_list[cmd].require_officer and not command_list[cmd].require_guild:
                     message += '[' + ', '.join(cmd) + '] - ' + command_list[cmd].get_description() + '\n'
 
-        message += '\n ПРИМЕЧАНИЕ: После использования, сообщение с командой автоматически удаляется, чтобы уменьшить количество флуда'
+        # message += '\n ПРИМЕЧАНИЕ: После использования, сообщение с командой автоматически удаляется, чтобы уменьшить количество флуда'
         message += f'\n За идеями/ошибками/вопросами обращаться [id{creator_id}|сюда], желательно с приставкой "по котику" или что-то в этом роде'
         bot.api.send_chat_msg(event.chat_id, message)
         return
@@ -77,12 +77,14 @@ class Notes(Command):
 
     def __init__(self):
         super().__init__(__class__.__name__, ('заметки', 'notes', 'note', 'правила'))
-        self.desc = 'Правила и остальные статьи'
+        self.desc = 'Правила и остальные статьи. Только для членов гильдии'
         # self.set_active(False)
         return
 
     def run(self, bot: VkBot, event: VkBotEvent):
-        # TODO get kbd and message
+        if event.message.from_id not in bot.api.get_members(GUILD_CHAT_ID):
+            return
+
         message = 'Заметки:'
         bot.api.send_chat_msg(event.chat_id, message, notes())
         return
@@ -97,33 +99,38 @@ class Balance(Command):
         return
 
     def run(self, bot: VkBot, event: VkBotEvent):
-        if event.message.from_id in bot.api.get_members(GUILD_CHAT_ID):
+        if event.message.from_id not in bot.api.get_members(GUILD_CHAT_ID):
+            return
 
-            if event.message.from_id == GUILD_LIBRARIAN_ID or event.message.from_id in users.get_leaders():
-                if 'reply_message' in event.message.keys():
-                    balance = users.get_balance(event.message.reply_message['from_id'])
-                    message = f"Счет игрока: {balance}" if balance is not None else "Нет записей, пусть сдаст профиль"
-                    bot.api.send_chat_msg(event.chat_id, message)
+        if event.message.from_id == GUILD_LIBRARIAN_ID or event.message.from_id in users.get_leaders():
+            if 'reply_message' in event.message.keys():
+                balance = users.get_balance(event.message.reply_message['from_id'])
+                message = f"Счет игрока: {balance}" if balance is not None else "Нет записей, пусть сдаст профиль"
+                bot.api.send_chat_msg(event.chat_id, message)
+                return
+
+            elif len(event.message.text.split(' ')) == 2:
+                if event.message.text.split(' ')[1] != 'все':
                     return
-                elif len(event.message.text.split(' ')) > 1:
-                    if event.message.text.split(' ')[1] == 'все':
-                        msg_id = bot.api.send_chat_msg(event.chat_id, 'Собираю информацию')[0]
-                        balance = users.get_all_balance()
-                        message = f'Баланс игроков гильдии {GUILD_NAME}:'
-                        for member in bot.api.get_members(GUILD_CHAT_ID):
-                            if member in balance.keys():
-                                message += f"\n@id{member}: {balance[member]}{gold}"
 
-                        bot.api.send_user_msg(event.message.from_id, message)
-                        bot.api.edit_msg(msg_id['peer_id'], msg_id['conversation_message_id'], 'Отправил список в лс')
-                        return
+                msg_id = bot.api.send_chat_msg(event.chat_id, 'Собираю информацию')[0]
+                balance = users.get_all_balance()
+                message = f'Баланс игроков гильдии {GUILD_NAME}:\n'
 
-            balance = users.get_balance(event.message.from_id)
-            if balance is not None:
-                message = f"Ваш долг: {gold}{-balance}(Положить {commission_price(-balance)})" if balance < 0 else f"Сейчас на счету: {gold}{balance}"
-            else:
-                message = "Хм... О вас нет записей, покажите профиль хотя бы раз!!"
+                message += '\n'.join(f"@id{member}: {balance[member]}{gold}"
+                                     for member in bot.api.get_members(GUILD_CHAT_ID)
+                                     if member in balance)
 
-            bot.api.send_chat_msg(event.chat_id, message)
+                bot.api.send_user_msg(event.message.from_id, message)
+                bot.api.edit_msg(msg_id['peer_id'], msg_id['conversation_message_id'], 'Отправил список в лс')
+                return
+
+        balance = users.get_balance(event.message.from_id)
+        if balance is not None:
+            message = f"Ваш долг: {gold}{-balance}(Положить {commission_price(-balance)})" if balance < 0 else f"Сейчас на счету: {gold}{balance}"
+        else:
+            message = "Хм... О вас нет записей, покажите профиль хотя бы раз!!"
+
+        bot.api.send_chat_msg(event.chat_id, message)
 
         return
