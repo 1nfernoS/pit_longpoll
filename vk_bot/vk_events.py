dadbfa7f3b4fff1b238d0ed8c0e4911f5898feea
1nfernoS <no_mail@mail.com>
2023-03-09 00:58:35 +0300

    Minor prettify in vk events

--- vk_bot/vk_events.py
+++ vk_bot/vk_events.py
@@ -2,14 +2,15 @@ from vk_api.bot_longpoll import VkBotEventType, VkBotEvent
 
 
 class VkEvent:
-    TYPES = [event.name for event in VkBotEventType.mro()[0]]
+    TYPES = [event.name for event in VkBotEventType]
 
     def __init__(self):
         for event_type in self.TYPES:
             setattr(self, event_type, self._empty_handler)
         return
 
-    def _empty_handler(self, cls, event: VkBotEvent) -> None:
+    @staticmethod
+    def _empty_handler(cls, event: VkBotEvent) -> None:
         print(f"No handler for event {event.type.name}")
         return
 
