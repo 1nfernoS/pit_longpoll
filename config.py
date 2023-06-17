49ee6f1e651a346246dfada54556981dc09247e6
1nfernoS <gaydenko01@yandex.ru>
2023-06-17 20:00:44 +0300

    Cleaning code from config imports

--- config.py
+++ config.py
@@ -6,27 +6,6 @@ from sys import argv
 from json import loads
 from dotenv import load_dotenv
 
-# branch-depended
-group_token: str = ''
-db_data: dict = dict()
-# whitelist of chat id's for bot to listen
-ALLOWED_CHATS: Tuple[int, ...] = tuple(int(i) for i in env.get('ALLOWED_CHATS').split(','))
-
-
-def load(branch_name: str):
-    if not load_dotenv('.env.' + branch_name):
-        print(f"No branch '{branch_name}', loaded 'dev' branch")
-        branch_name = 'dev'
-
-    if not load_dotenv('.env.' + branch_name):
-        raise EnvironmentError(f'No .env.{branch_name} file')
-
-    global group_token, db_data
-
-    group_token = env.get('group_token')
-    db_data = loads(env.get('db_data'))
-    return
-
 
 # Load constants
 PIT_BOT: int = -182985865
@@ -54,6 +33,30 @@ APO_PAYMENT: int = int(env.get('PAYMENT_APO'))
 
 creator_id: int = int(env.get('CREATOR_ID'))
 
+
+# branch-depended
 branch = env.get('BRANCH')
+
+group_token: str = ''
+db_data: dict = dict()
+# whitelist of chat id's for bot to listen
+ALLOWED_CHATS: Tuple[int, ...] = tuple()
+
+
+def load(branch_name: str):
+    if not load_dotenv('.env.' + branch_name):
+        print(f"No branch '{branch_name}', loaded 'dev' branch")
+        branch_name = 'dev'
+
+    if not load_dotenv('.env.' + branch_name):
+        raise EnvironmentError(f'No .env.{branch_name} file')
+
+    global group_token, db_data, ALLOWED_CHATS
+
+    group_token = env.get('group_token')
+    db_data = loads(env.get('db_data'))
+    ALLOWED_CHATS = tuple(int(i) for i in env.get('ALLOWED_CHATS').split(','))
+    return
+
 
 load(branch)
