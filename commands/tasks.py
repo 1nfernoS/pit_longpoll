246b9fc9fe3ee87df9f4c954ac61feafadee36b7
1nfernoS <gaydenko01@yandex.ru>
2023-10-06 14:28:10 +0300

    Fix time zone bug on Remind

--- commands/tasks.py
+++ commands/tasks.py
@@ -34,7 +34,7 @@ class Remind(Command):
             'text': ' '.join(event.message.text.split()[1:]),
             'msg_id': bot.api.get_conversation_msg(event.message.peer_id, event.message.conversation_message_id)['id']
         }
-        Task(datetime.now() + timedelta(hours=1), remind, args).add()
+        Task(datetime.utcnow() + timedelta(hours=1+3), remind, args).add()
 
         bot.api.send_chat_msg(event.chat_id, 'Хорошо, напомню через часик')
         s.close()
