0279da383ae951c61005c750ae05b7a0cdca764b
1nfernoS <no_mail@mail.com>
2024-01-14 13:08:28 +0300

    Added new answers to books

--- commands/tasks.py
+++ commands/tasks.py
@@ -7,7 +7,7 @@ from ORM import session, UserInfo, Task, Logs, Notes
 
 from tasks.exec_task import remind
 
-from typing import TYPE_CHECKING
+from typing import List, TYPE_CHECKING
 if TYPE_CHECKING:
     from vk_api.bot_longpoll import VkBotEvent
     from vk_bot.vk_bot import VkBot
@@ -63,7 +63,7 @@ class Announce(Command):
         deletion = ('удалить', 'delete', 'remove')
         listing = ('мои', 'список', 'list')
 
-        msg = event.message.text.split()
+        msg: List[str] = event.message.text.split()
         if len(msg) < 2:
             bot.api.send_chat_msg(event.chat_id, "Не хватает аргументов")
             s.close()
@@ -81,13 +81,13 @@ class Announce(Command):
         Logs(event.message.from_id, __class__.__name__, reason=event.message.text[:200]+'...').make_record()
 
         if msg[1].lower() in adding:
-            note = ' '.join(msg[2:])
-            if len(note) > 255:
+            note_text: str = ' '.join(msg[2:])
+            if len(note_text) > 255:
                 bot.api.send_chat_msg(event.chat_id,
-                                      f"Максимум 255 символов в объявлении, у вас {len(note)} (эмодзи едят по 6-10 символов)")
+                                      f"Максимум 255 символов в объявлении, у вас {len(note_text)} (эмодзи едят по 6-10 символов)")
                 s.close()
                 return
-            Notes(event.message.from_id, note).create()
+            Notes(event.message.from_id, note_text).create()
             bot.api.send_chat_msg(event.chat_id, "Добавил твое объявление в газету!")
 
         elif msg[1].lower() in deletion:
@@ -97,29 +97,29 @@ class Announce(Command):
                 bot.api.send_chat_msg(event.chat_id, "Для удаления объявления ме нужен его номер")
                 s.close()
                 return
-            note: Notes = s.query(Notes).filter(Notes.note_id == note_id).first()
-            if not note:
+            note_text: List[Notes] = s.query(Notes).filter(Notes.note_id == note_id).first()
+            if not note_text:
                 bot.api.send_chat_msg(event.chat_id, "Не могу найти объявление")
                 s.close()
                 return
-            if note.note_author != event.message.from_id and not user.user_role.role_can_utils:
+            if note_text.note_author != event.message.from_id and not user.user_role.role_can_utils:
                 bot.api.send_chat_msg(event.chat_id, "Это не твое объявление!")
                 s.close()
                 return
 
-            note.is_active = False
-            s.add(note)
+            note_text.is_active = False
+            s.add(note_text)
             s.commit()
             bot.api.send_chat_msg(event.chat_id, "Объявление удалено!")
         elif msg[1].lower() in listing:
-            notes: Notes = s.query(Notes).filter(Notes.note_author == user.user_id, Notes.is_active==True).all()
+            notes: List[Notes] = s.query(Notes).filter(Notes.note_author == user.user_id, Notes.is_active==True).all()
             if not notes:
                 bot.api.send_chat_msg(event.chat_id, 'У вас сейчас нет объявлений!')
                 s.close()
                 return
             answer = f"Ваши объявления:"
-            for note in notes:
-                answer += f"\n{note.note_id} -{e.tab}{note.note_text} (до {note.expires_in})"
+            for note_text in notes:
+                answer += f"\n{note_text.note_id} -{e.tab}{note_text.note_text} (до {note_text.expires_in})"
             bot.api.send_chat_msg(event.chat_id, answer)
 
         s.close()
