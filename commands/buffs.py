cdc5ff3f46c66ad9cfe54e6d967f8ec5c60a1594
1nfernoS <no_mail@mail.com>
2024-11-03 15:46:51 +0300

    Added voices and active check for other buffers, not only apostols

--- commands/buffs.py
+++ commands/buffs.py
@@ -7,6 +7,8 @@ from config import creator_id
 from utils import keyboards
 from dictionaries.buffs import APOSTOL_ITEM_ID, WARLOCK_ITEM_ID, PALADIN_ITEM_ID, CRUSADER_ITEM_ID, LIGHT_INC_ITEM_ID
 
+from profile_api import get_voices
+
 if TYPE_CHECKING:
     from vk_bot.vk_bot import VkBot
     from vk_api.bot_longpoll import VkBotEvent
@@ -58,7 +60,6 @@ class Apostol(Command):
 
         Logs(event.message.from_id, __class__.__name__).make_record()
 
-        from profile_api import get_voices
 
         apostols: List[BuffUser] = s.query(BuffUser).filter(BuffUser.buff_type_id == APOSTOL_ITEM_ID,
                                                             BuffUser.buff_user_is_active == 1).all()
@@ -99,17 +100,26 @@ class Warlock(Command):
 
         Logs(event.message.from_id, __class__.__name__).make_record()
 
-        warlocks: List[BuffUser] = s.query(BuffUser).filter(BuffUser.buff_type_id == WARLOCK_ITEM_ID).all()
+        warlocks: List[BuffUser] = s.query(BuffUser).filter(BuffUser.buff_type_id == WARLOCK_ITEM_ID,
+                                                            BuffUser.buff_user_is_active == 1).all()
 
         if not warlocks:
             bot.api.send_chat_msg(event.chat_id, "Мне жаль, но сейчас нет ни одного активного чернокнижника")
             return
-
+        
+        voices = {buffer.buff_user_id: get_voices(buffer.buff_user_profile_key, buffer.buff_user_id)
+                  for buffer in warlocks}
+        
+        if not any(voices.values()):
+            bot.api.send_chat_msg(event.chat_id, "Мне жаль, но сейчас нет ни одного чернокнижника с голосами")
+            return
+        
         msg_id = event.message.conversation_message_id
         for buffer in warlocks:
 
             kbd = keyboards.warlock(buffer.buff_user_id, msg_id, event.chat_id)
-            bot.api.send_chat_msg(event.chat_id, f"[id{buffer.buff_user_id}|Чернокнижник]", kbd=kbd)
+            bot.api.send_chat_msg(event.chat_id,
+                                  f"[id{buffer.buff_user_id}|Чернокнижник]: {voices[buffer.buff_user_id]}", kbd=kbd)
         s.close()
         return
 
@@ -130,12 +140,23 @@ class PaladinStuff(Command):
 
         Logs(event.message.from_id, __class__.__name__).make_record()
 
-        paladins: List[BuffUser] = s.query(BuffUser).filter(BuffUser.buff_type_id == PALADIN_ITEM_ID).all()
-        crusaders: List[BuffUser] = s.query(BuffUser).filter(BuffUser.buff_type_id == CRUSADER_ITEM_ID).all()
-        light_incs: List[BuffUser] = s.query(BuffUser).filter(BuffUser.buff_type_id == LIGHT_INC_ITEM_ID).all()
+        paladins: List[BuffUser] = s.query(BuffUser).filter(BuffUser.buff_type_id == PALADIN_ITEM_ID,
+                                                            BuffUser.buff_user_is_active == 1).all()
+        crusaders: List[BuffUser] = s.query(BuffUser).filter(BuffUser.buff_type_id == CRUSADER_ITEM_ID,
+                                                            BuffUser.buff_user_is_active == 1).all()
+        light_incs: List[BuffUser] = s.query(BuffUser).filter(BuffUser.buff_type_id == LIGHT_INC_ITEM_ID,
+                                                            BuffUser.buff_user_is_active == 1).all()
 
         if not any([paladins, crusaders, light_incs]):
             bot.api.send_chat_msg(event.chat_id, "Мне жаль, но сейчас нет ни одного активного паладина")
+            return
+        
+
+        voices = {buffer.buff_user_id: get_voices(buffer.buff_user_profile_key, buffer.buff_user_id)
+                  for buffer in paladins+crusaders+light_incs}
+
+        if not any(voices.values()):
+            bot.api.send_chat_msg(event.chat_id, "Мне жаль, но сейчас нет ни одного чернокнижника с голосами")
             return
 
         msg_id = event.message.conversation_message_id
@@ -143,14 +164,17 @@ class PaladinStuff(Command):
         if paladins:
             for buffer in paladins:
                 kbd = keyboards.paladin(buffer.buff_user_id, msg_id, event.chat_id)
-                bot.api.send_chat_msg(event.chat_id, f"[id{buffer.buff_user_id}|Паладин]", kbd=kbd)
+                bot.api.send_chat_msg(event.chat_id,
+                                      f"[id{buffer.buff_user_id}|Паладин]: {voices[buffer.buff_user_id]}", kbd=kbd)
         if crusaders:
             for buffer in crusaders:
                 kbd = keyboards.crusader(buffer.buff_user_id, msg_id, event.chat_id)
-                bot.api.send_chat_msg(event.chat_id, f"[id{buffer.buff_user_id}|Крестоносец]", kbd=kbd)
+                bot.api.send_chat_msg(event.chat_id,
+                                      f"[id{buffer.buff_user_id}|Крестоносец]: {voices[buffer.buff_user_id]}", kbd=kbd)
         if light_incs:
             for buffer in light_incs:
                 kbd = keyboards.light_inc(buffer.buff_user_id, msg_id, event.chat_id)
-                bot.api.send_chat_msg(event.chat_id, f"[id{buffer.buff_user_id}|Воплощение света]", kbd=kbd)
+                bot.api.send_chat_msg(event.chat_id,
+                                      f"[id{buffer.buff_user_id}|Воплощение света]: {voices[buffer.buff_user_id]}", kbd=kbd)
         s.close()
         return
